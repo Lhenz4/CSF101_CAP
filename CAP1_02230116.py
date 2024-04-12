c93e96307ce4bd6148bd36f23820b9bbcae2df9f
unknown <02230116.cst@rub.edu.bt>
2024-04-12 21:40:22 +0600

    Third Update: Rock Paper Scissor game score from given file

--- CAP1_02230116.py
+++ CAP1_02230116.py
@@ -1,4 +1,4 @@
- ##################################################################################
+##################################################################################
 # Name: Ugyen Lhendup
 # Course: First Year B.E in ECE
 # Student ID Number: 02230116
@@ -9,7 +9,7 @@
 # https://chat.openai.com/
 #####################################################################################
 # SOLUTION
-# Solution Score: 50023
+# Solution Score: 50223
 ######################################################################################
 
 # Firstly I defined the function named read_input. 
@@ -25,10 +25,10 @@ def read_input(f_name):
             # As the file contain two letters in a line, first for the shape (rock,paper or scissor) and
             # second for outcome i.e whether to lose, win or draw, I splited it and assigned them to 
             # the variable named shape and outcome 
-            shape, outcome = line.split()
+            rps_shape, outcome_condition = line.split()
             # Here round_result is been updated by appending the splitted line. I appeneded splitted lines i.e
             # shape and outcome in list. Therefore, round_result will have nested list which becomes 2 column array
-            round_results.append([shape, outcome])
+            round_results.append([rps_shape, outcome_condition])
     # This returns the final items stored in round_results. 
     return round_results
 #As a whole, read_input function will return the array stored in round_result
@@ -41,29 +41,33 @@ def calculate_score(results):
     t_score = 0
     # To calculate the score, the result array is iterated and following comparison are done to update
     # the score accordingly in t_score variable
-    # |shape|        |score|           |outcome|    |score|
-    #  A (Rock)        +1               X (Lose)      +0       
-    #  B (Paper)       +2               Y (Draw)      +3
-    #  C (Scissor)     +3               Z (Win)       +6
-    for shape, outcome in results:
-        #shape scores
-        if shape == "A":
-            shape_score = 1
-        elif shape == "B":
-            shape_score = 2
-        elif shape == "C":
-            shape_score = 3
+    for rps_shape, outcome_condition in results:
+        if rps_shape == "A": # comparison for A (rock) and its combination
+            if outcome_condition == "X":
+                earned_score = 3
+            elif outcome_condition == "Y":
+                earned_score = 4
+            else:
+                earned_score = 8
 
-        #outcome scores
-        if outcome == "X":
-            outcome_score = 0
-        elif outcome == "Y":
-            outcome_score = 3
-        elif outcome == "Z":
-            outcome_score = 6
+        elif rps_shape == "B": # comparison for B (Paper) and its combination
+            if outcome_condition == "X":
+                earned_score = 1
+            elif outcome_condition == "Y":
+                earned_score = 5
+            else:
+                earned_score = 9
+
+        else:  # comparison for C (scissors) and its combination
+            if outcome_condition == "X":
+                earned_score = 2
+            elif outcome_condition == "Y":
+                earned_score = 6
+            else:
+                earned_score = 7
         
         # Here the score is updated till the end of the loop
-        t_score += shape_score   + outcome_score
+        t_score += earned_score
     # Then final total score is returned
     return t_score
 # In sumarry, the calculate_score function returns the total sum of the score
@@ -78,3 +82,4 @@ score = calculate_score(results)
 print(f"Total Score: {score}")
 
 
+ 
